51dd242b668092bdfb8d7218ce1d29753c2190cf
Vincent Lambert <vincent.lambert@etudiant.univ-lr.fr>
2020-03-22 17:16:19 +0100

    Ajout de commentaire pour la génération de la documentation, fait avec Emmanuel CHENUAUD.

--- projet-NATUITION/SensorProcessing.py
+++ projet-NATUITION/SensorProcessing.py
@@ -4,175 +4,223 @@ import threading
 import time
 import re
 from Database import Database
-
+from AnalogDigitalConverter import AnalogDigitalConverter
 class SensorProcessing:
-    def __init__(self,pathConfig,analogDigitalConverter):
-        
-        self.pathConfig = pathConfig + '/config.data'
-        
-        if(not path.exists(self.pathConfig)):
-            self.initFileConfiguration()
-        
-        self.analogDigitalConverter = analogDigitalConverter
-        
-        host = self.getPropertyInFileConfiguration("Host")
-        dbName = self.getPropertyInFileConfiguration("Database\ name")
-        login = self.getPropertyInFileConfiguration("Login")
-        password = self.getPropertyInFileConfiguration("Password")
-        port = self.getPropertyInFileConfiguration("Port")
-        
-        self.database = Database(host,dbName,login,password,port)
-        
-        
-    def startSession(self):
-        surname = self.getPropertyInFileConfiguration("Surname")
-        name = self.getPropertyInFileConfiguration("Name")
-        #insertClient() of Database class
-        self.database.insertClient(surname,name)
-        
-        #startSession() of Database class
-        self.database.startSession()
-        
-        #startThread
-        self.thread = threading.Thread(target=self.loop , args=({0.5}))
-        self.thread.start()
-        
-    def endSession(self):
-        #stopThread
-        self.thread.do_run = False
-        self.thread.join()
-        
-        #endSession() of Database class
-        self.database.endSession()
-        
-    # Define a function for the thread
-    def loop(self,delay):
-        t = threading.currentThread()
-        while getattr(t, "do_run", True):
-            print("Wheels degree : "+str(self.getAngle()))
-            #insertResults(self.getAngle()) of Database class
-            self.database.insertResults(self.getAngle())
-            time.sleep(delay)
-            
-    def getAngle(self):
-        voltage = self.analogDigitalConverter.getFormattedVoltage()
-        print(voltage)
-        turns = float(self.getPropertyInFileConfiguration("Turns"))
-        zero = float(self.getPropertyInFileConfiguration("0°"))
-        ratioMP = float(self.getPropertyInFileConfiguration("Ratio\ M&P"))
-        ratioMW = float(self.getPropertyInFileConfiguration("Ratio\ M&W"))
-        reference = float(self.getPropertyInFileConfiguration("Reference"))
-        
-        voltageSigne = (voltage - zero)
-        
-        minusDegree = zero / (180*turns)
-        plusDegree = (reference-zero) / (180*turns)
-        
-        potentiometreDegree = voltageSigne/plusDegree
-        if(voltageSigne < zero):
-            potentiometreDegree = voltageSigne/minusDegree
-                        
-        motorDegree = potentiometreDegree / ratioMP
-        wheelsDegree = motorDegree / ratioMW
+	"""
+		Class managing the sensor's class and the database's class. 
+	"""
+		
+	def __init__(self,pathConfig,channel):
+		"""
+			Inits SensorProcessing with path of the configuration file.\n
+			Create an instance of AnalogDigitalConverter, Database and thread.\n
+			And if configuration file doesn't exist he create it.
 
-        return float("{0:.3f}".format(wheelsDegree))
-        
-    def getPropertyInFileConfiguration(self,name):
-        file = open(self.pathConfig, 'r')
-        reg = '^'+name+':\ (.*)'
-        for line in file.readlines():
-            matchObj = re.match( reg, line, re.X|re.I)
-            if(matchObj is not None):
-                return matchObj.group(1)
-        
-    def initFileConfiguration(self):
-        file = open(self.pathConfig,'w')
-        
-        #   # --------------------------------------------------------------------------------------------------------------
-        file.write("# --------------------------------------------------------------------------------------------------------------\n")
-        #   # | Configuration file for the purple robot retrieving the steering angle project and saving it to a database. |
-        file.write("# | Configuration file for the purple robot retrieving the steering angle project and saving it to a database. |\n")
-        #   # | To comment in this file use '#'.                                                                           |
-        file.write("# | To comment in this file use '#'.                                                                           |\n")
-        #   # --------------------------------------------------------------------------------------------------------------
-        file.write("# --------------------------------------------------------------------------------------------------------------\n\n")
+			:param pathConfig: Path of the configuration file.    
+			:param channel: Channel number where the potentiometer is connected [0|1|2].
+			
+			**Authors of this class :** CHENUAUD Emmanuel and LAMBERT Vincent.\n  
+		"""
+
+		self.pathConfig = pathConfig + '/config.data'
+		
+		if(not path.exists(self.pathConfig)):
+			self.initFileConfiguration()
+
+		host = self.getPropertyInFileConfiguration("Host")
+		dbName = self.getPropertyInFileConfiguration("Database\ name")
+		login = self.getPropertyInFileConfiguration("Login")
+		password = self.getPropertyInFileConfiguration("Password")
+		port = self.getPropertyInFileConfiguration("Port")
 
 
-        #   # ------------------------------------------
-        file.write("# ------------------------------------------\n")
-        #   # | Analog digital converter configuration |
-        file.write("# | Analog digital converter configuration |\n")
-        #   # ------------------------------------------
-        file.write("# ------------------------------------------\n\n")
+		self.thread = threading.Thread(target=self.loop , args=({0.5}))
+		self.analogDigitalConverter = AnalogDigitalConverter(channel)
+		self.database = Database(host,dbName,login,password,port)
+		
+	def startSession(self):
+		"""
+			Lauch robot session.\n
+			This method :
+				* create in database the robot if doesn't exist,
+				* start session in Database,
+				* start the thread.
 
-        #   # The voltage in volt of the zero angle :
-        file.write("# The voltage in volt of the zero angle :\n")
-        #   0°: value
-        file.write("0°: 2.047\n")
-        #   # Potentiometer reference voltage :
-        file.write("# Potentiometer reference voltage :\n")
-        #   Reference: value
-        file.write("Reference: 4.094\n")
-        #   # Maximum number of turns that the potentiometer can make :
-        file.write("# Maximum number of turns that the potentiometer can make :\n")
-        #   Turns: value
-        file.write("Turns: 1\n")
-        #   # Multiplication ratio between the motor and the potentiometer :
-        file.write("# Multiplication ratio between the motor and the potentiometer :\n")
-        #   Ratio M&P: value
-        file.write("Ratio M&P: 3\n")
-        #   # Multiplication ratio between the motor and wheels :
-        file.write("# Multiplication ratio between the motor and wheels :\n")
-        #   Ratio M&W: value
-        file.write("Ratio M&W: 3.6\n\n")
+		"""
 
-        #   # ------------------------
-        file.write("# ------------------------\n")
-        #   # | Customer information |
-        file.write("# | Customer information |\n")
-        #   # ------------------------
-        file.write("# ------------------------\n\n")
+		robotSerialNumber = self.getPropertyInFileConfiguration("Serial\ number")
 
-        #   # The customer's name :
-        file.write("# The customer's name :\n")
-        #   Name: value
-        file.write("Name: value\n\n")
+		#insertRobot() of Database class
+		self.database.insertRobot(robotSerialNumber)
+		
+		#startSession() of Database class
+		self.database.startSession()
+		
+		#startThread
+		self.thread.start()
+		
+	def endSession(self):
+		"""
+			Stop robot session.\n    
+			This method :
+				* stop session in Database,
+				* stop the thread.
 
-        #   # The customer's surname :
-        file.write("# The customer's surname :\n")
-        #   Surname: value
-        file.write("Surname: value\n\n")
+		"""
 
-        #   # ------------------------
-        file.write("# ------------------------\n")
-        #   # | Database information |
-        file.write("# | Database information |\n")
-        #   # ------------------------
-        file.write("# ------------------------\n\n")
+		#stopThread
+		self.thread.do_run = False
+		self.thread.join()
+		
+		#endSession() of Database class
+		self.database.endSession()
+		
+	def loop(self,delay):
+		"""
+			Loop of robot's thread.    
 
-        #   # The host of database :
-        file.write("# The host of database :\n")
-        #   Host: value
-        file.write("Host: value\n\n")
-        
-        #   # The port of database :
-        file.write("# The port of database :\n")
-        #   Port: value
-        file.write("Port: value\n\n")
+			:param delay: Waiting time between each iteration (in millisecond). 
+		"""
 
-        #   # The name of database user :
-        file.write("# The name of database user :\n")
-        #   Login: value
-        file.write("Login: value\n\n")
+		t = threading.currentThread()
+		while getattr(t, "do_run", True):
+			print("Wheels degree : "+str(self.getAngle()))
+			self.database.insertResults(self.getAngle())
+			time.sleep(delay)
+			
+	def getAngle(self):
+		"""
+			Allows to recover the value of wheels' angle (in degree).   
 
-        #   # The password of database user :
-        file.write("# The password of database user :\n")
-        #   Password: value
-        file.write("Password: value\n\n")
-        
-        #   # The database name :
-        file.write("# The database name :\n")
-        #   Database name: value
-        file.write("Database name: value\n")
+			:return: float: Wheels' angle (in degree), postive degrees : wheels turn right and negative degrees for left.
+		"""
 
-        file.close() 
+		voltage = self.analogDigitalConverter.getFormattedVoltage()
+		print(voltage)
+		turns = float(self.getPropertyInFileConfiguration("Turns"))
+		zero = float(self.getPropertyInFileConfiguration("0°"))
+		ratioMP = float(self.getPropertyInFileConfiguration("Ratio\ M&P"))
+		ratioMW = float(self.getPropertyInFileConfiguration("Ratio\ M&W"))
+		reference = float(self.getPropertyInFileConfiguration("Reference"))
+		
+		voltageSigne = (voltage - zero)
+		
+		minusDegree = zero / (180*turns)
+		plusDegree = (reference-zero) / (180*turns)
+		
+		potentiometreDegree = voltageSigne/plusDegree
+		if(voltageSigne < zero):
+			potentiometreDegree = voltageSigne/minusDegree
+						
+		motorDegree = potentiometreDegree / ratioMP
+		wheelsDegree = motorDegree / ratioMW
+
+		return float("{0:.3f}".format(wheelsDegree))
+		
+	def getPropertyInFileConfiguration(self,name):
+		"""
+			Allows to recover a value of property in file configuration.
+
+			:param name: Name of property in file configuration.
+
+			:return: str: Property value.
+        """
+
+		file = open(self.pathConfig, 'r')
+		reg = '^'+name+':\ (.*)'
+		for line in file.readlines():
+			matchObj = re.match( reg, line, re.X|re.I)
+			if(matchObj is not None):
+				return matchObj.group(1)
+		
+	def initFileConfiguration(self):
+		"""
+			Allows to create blank file configuration.\n 
+			Fields with the value 'value' must be completed.
+        """
+
+		file = open(self.pathConfig,'w')
+		
+		#   # --------------------------------------------------------------------------------------------------------------
+		file.write("# --------------------------------------------------------------------------------------------------------------\n")
+		#   # | Configuration file for the purple robot retrieving the steering angle project and saving it to a database. |
+		file.write("# | Configuration file for the purple robot retrieving the steering angle project and saving it to a database. |\n")
+		#   # | To comment in this file use '#'.                                                                           |
+		file.write("# | To comment in this file use '#'.                                                                           |\n")
+		#   # --------------------------------------------------------------------------------------------------------------
+		file.write("# --------------------------------------------------------------------------------------------------------------\n\n")
+
+
+		#   # ------------------------------------------
+		file.write("# ------------------------------------------\n")
+		#   # | Analog digital converter configuration |
+		file.write("# | Analog digital converter configuration |\n")
+		#   # ------------------------------------------
+		file.write("# ------------------------------------------\n\n")
+
+		#   # The voltage in volt of the zero angle :
+		file.write("# The voltage in volt of the zero angle :\n")
+		#   0°: value
+		file.write("0°: value\n")
+		#   # Potentiometer reference voltage :
+		file.write("# Potentiometer reference voltage :\n")
+		#   Reference: value
+		file.write("Reference: value\n")
+		#   # Maximum number of turns that the potentiometer can make :
+		file.write("# Maximum number of turns that the potentiometer can make :\n")
+		#   Turns: value
+		file.write("Turns: value\n")
+		#   # Multiplication ratio between the motor and the potentiometer :
+		file.write("# Multiplication ratio between the motor and the potentiometer :\n")
+		#   Ratio M&P: value
+		file.write("Ratio M&P: value\n")
+		#   # Multiplication ratio between the motor and wheels :
+		file.write("# Multiplication ratio between the motor and wheels :\n")
+		#   Ratio M&W: value
+		file.write("Ratio M&W: value\n\n")
+
+		#   # ------------------------
+		file.write("# ------------------------\n")
+		#   # | Robot information |
+		file.write("# | Robot information |\n")
+		#   # ------------------------
+		file.write("# ------------------------\n\n")
+
+		#   # The robot's serial number :
+		file.write("# The robot's serial number :\n")
+		#   Serial\ number: value
+		file.write("Serial number: value\n\n")
+
+		#   # ------------------------
+		file.write("# ------------------------\n")
+		#   # | Database information |
+		file.write("# | Database information |\n")
+		#   # ------------------------
+		file.write("# ------------------------\n\n")
+
+		#   # The host of database :
+		file.write("# The host of database :\n")
+		#   Host: value
+		file.write("Host: value\n\n")
+		
+		#   # The port of database :
+		file.write("# The port of database :\n")
+		#   Port: value
+		file.write("Port: value\n\n")
+
+		#   # The name of database user :
+		file.write("# The name of database user :\n")
+		#   Login: value
+		file.write("Login: value\n\n")
+
+		#   # The password of database user :
+		file.write("# The password of database user :\n")
+		#   Password: value
+		file.write("Password: value\n\n")
+		
+		#   # The database name :
+		file.write("# The database name :\n")
+		#   Database name: value
+		file.write("Database name: value\n")
+
+		file.close() 
--- projet-NATUITION/emulatorGPS/EmulateGPS.py
+++ projet-NATUITION/emulatorGPS/EmulateGPS.py
@@ -1,5 +1,19 @@
 class EmulateGPS:
+	"""
+		This class emulate a route between UIT of La Rochelle and Natuition's office.\n
+		This route is composed by one thousand point. One point is defiend by a latitude and a longitude.\n
+		This class has to be used to test inserting potentiometer results in database whitout use the robot.
+	"""
 	def __init__(self):
+		"""
+			This method initialize the route parameters : 
+				* start point of route,
+				* end point of route.
+
+			These values can be modified to create other routes (it's useless).
+
+			**Authors of this class :** CHENUAUD Emmanuel.
+		"""
 		self.latitude_start = 46.141993
 		self.longitude_start = -1.151800
 
@@ -14,10 +28,21 @@ class EmulateGPS:
 		self.counter = 0
 
 	def calculateCoeff(self):
+		"""
+			This method calculates spacing between two route points for routes composed by one thousand points :
+				* latitude coefficient,
+				* longitude coefficient.
+
+		"""
 		self.coef_latitude = abs(abs(self.latitude_end)-abs(self.latitude_start))/1000
 		self.coef_longitude = abs(abs(self.longitude_end)-abs(self.longitude_start))/1000
 
 	def getLatitude(self):
+		"""
+			Get the latitude of current point.
+
+			:return: float: The latitude of current point.
+		"""
 		if self.latitude_start<self.latitude_end :
 			self.curr_latitude += self.coef_latitude
 		else:
@@ -25,6 +50,11 @@ class EmulateGPS:
 		return self.curr_latitude
 
 	def getLongitude(self):
+		"""
+			Get the longitude of current point.
+
+			:return: float: The longitude of current point.
+		"""
 		if self.longitude_start<self.longitude_end :
 			self.curr_longitude += self.coef_longitude
 		else:
@@ -33,9 +63,18 @@ class EmulateGPS:
 		return self.curr_longitude
 
 	def countIncrement(self):
+		"""
+			This method increments the position on the route by one.\n
+			Don't use it method. It's already used in getLongitude().
+		"""
 		self.counter += 1
 
 	def getLatitude_N(self,N):
+		"""
+			Get the latitude of the N-th point in the route.
+
+			:param N: The latitude of targeted point.
+		"""
 		if self.latitude_start<self.latitude_end:
 			latitudeN = self.latitude_start + N*self.coef_latitude
 		else:
@@ -43,6 +82,11 @@ class EmulateGPS:
 		return latitudeN
 
 	def getLongitude_N(self,N):
+		"""
+			Get the longitude of the N-th point in the route.
+
+			:param N: The longitude of targeted point.
+		"""
 		if self.longitude_start<self.longitude_end:
 			longitudeN = self.longitude_start + N*self.coef_longitude
 		else:
